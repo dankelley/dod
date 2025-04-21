3a4ecefd3b98d87066abc86548e40df5888583b4
dankelley <kelley.dan@gmail.com>
2025-04-21 11:23:09 -0300

    fix varname in a test

--- tests/testthat/test_ctd.R
+++ tests/testthat/test_ctd.R
@@ -5,7 +5,7 @@ test_that("ctd downloads", {
     dir.create(tempDir)
     expect_error(dod.ctd("invalid"), "'program' must be")
     expect_error(dod.ctd("BBMP"), "ID must be supplied")
-    expect_silent(dod.ctd(program = "BBMP", year = "2024", ID = 1, destdir = tempDir, quiet = TRUE))
+    expect_silent(dod.ctd(program = "BBMP", year = "2024", ID = "001_1", destdir = tempDir, quiet = TRUE))
     expect_error(dod.ctd("GTSPP"), "must provide basin")
     expect_error(dod.ctd("GTSPP", basin = "error"), "'arg' should be one of")
     expect_error(dod.ctd("ITP"), "ID must be supplied")
